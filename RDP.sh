28300f156a0f49fe3648836631015f0aea7fcbc0
smnahidofficial <66544659+smnahidofficial@users.noreply.github.com>
2021-02-27 07:31:45 +0600

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-printf "Conecting please wait..." >&2
+printf "Connecting please wait..." >&2
 {
-sudo useradd -m SOFTOWA
-sudo adduser SOFTOWA sudo
-echo 'SOFTOWA:8426' | sudo chpasswd
+sudo useradd -m ALOK
+sudo adduser ALOK sudo
+echo 'ALOK:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -24,12 +24,12 @@ sudo apt install nautilus nano -y
 sudo apt -y install obs-studio
 sudo apt -y install chrome
 sudo apt -y install firefox
-sudo adduser SOFTOWA chrome-remote-desktop
+sudo adduser ALOK chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - SOFTOWA -c """$CRP"""
+su - ALOK -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
 printf 'Your SUDO Pasword Is 8426 \n'
